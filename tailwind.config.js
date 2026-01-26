20376549af8575f68ab5fce5136ee8c6d31f0bd3
Amir Ghareeb <amirghareeb593@gmail.com>
2026-01-26 20:38:59 +0200

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     './app/**/*.{js,jsx,ts,tsx,mdx}',
-    './pages/**/*.{js,jsx,ts,tsx,mdx}',
     './components/**/*.{js,jsx,ts,tsx,mdx}',
     './context/**/*.{js,jsx,ts,tsx,mdx}',
     './hooks/**/*.{js,jsx,ts,tsx,mdx}',
